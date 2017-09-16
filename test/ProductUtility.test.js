650264d4907d0cba46f79249b73860d12a985270
art <partypeopleland@gmail.com>
2017-09-16 20:38:39 +0800

    ProductUtility加入計算商品總數量 - 紅燈

--- test/ProductUtility.test.js
+++ test/ProductUtility.test.js
@@ -2,21 +2,36 @@ var chai = require('chai')
 var should = chai.should()
 var ProductUtility = require('../src/ProductUtility.js');
 
-var dataSets = [
+var dataSetsPrice = [
     { args: [500, 1], expected: 500 },
     { args: [350, 3], expected: 1050 },
 ]
 
-describe('ProductUtility', function () {
-  
-  describe('#GetTotalAmount(products)', function () {
+var dataSetsCount = [
+    { args: [500, 1], expected: 1 },
+    { args: [350, 3], expected: 3 },
+]
 
-    dataSets.forEach((data)=> {
+describe('ProductUtility', function () {
+  describe('#GetTotalAmount(products)', function () {
+      dataSetsPrice.forEach((data)=> {
         it(`商品單價:${data.args[0]} x 數量:${data.args[1]} = 總金額:${data.expected}`, () => {
             var target = new ProductUtility()
             // Act
             var products = [{ Name: "愛心筆", Price: data.args[0], Qty: data.args[1] }]
             var actual = target.GetTotalAmount(products)
+            // Assert
+            actual.should.equal(data.expected);
+        });
+    });
+  });
+  describe('#GetTotalCount(products)', function () {
+      dataSetsCount.forEach((data) => {
+        it(`商品數量:${data.args[1]} = 總數量:${data.expected}`, () => {
+            var target = new ProductUtility()
+            // Act
+            var products = [{ Name: "愛心筆", Price: data.args[0], Qty: data.args[1] }]
+            var actual = target.GetTotalCount(products)
             // Assert
             actual.should.equal(data.expected);
         });
