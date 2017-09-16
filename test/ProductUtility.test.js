1cfc2a958a7c031e764a57cf5e3c7e13293ed717
art <partypeopleland@gmail.com>
2017-09-16 20:26:17 +0800

    商品總額計算 - 紅燈

---
+++ test/ProductUtility.test.js
@@ -0,0 +1,24 @@
+var chai = require('chai')
+var should = chai.should()
+var ProductUtility = require('../src/ProductUtility.js');
+
+var dataSets = [
+    { args: [500, 1], expected: 500 },
+]
+
+describe('ProductUtility', function () {
+  
+  describe('#GetTotalAmount(products)', function () {
+
+    dataSets.forEach((data)=> {
+        it(`商品單價:${data.args[0]} x 數量:${data.args[1]} = 總金額:${data.expected}`, () => {
+            var target = new ProductUtility()
+            // Act
+            var products = [{ Name: "愛心筆", Price: data.args[0], Qty: data.args[1] }]
+            var actual = target.GetTotalAmount(products)
+            // Assert
+            actual.should.equal(data.expected);
+        });
+    });
+  });
+});
